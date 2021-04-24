59dfc7749ee9c76244a75299f1e2d66e967dd194
veedahv <veedahv55@gmail.com>
2021-04-24 23:28:27 +0100

    new commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,39 +30,19 @@ export default {
   },
   mounted() {
     fetch(
-      "https://newsapi.org/v2/top-headlines?country=ng&apiKey=0bae2999e97a4e048d292e345367ca99"
+      "https://newsapi.org/v2/top-headlines?country=ng&apiKey=" + process.env.VUE_APP_BLOGKEY
     )
       .then((res) => res.json())
-      // .then((data) => console.log(data.articles))
       .then((data) => (this.posts = data.articles))
       .catch((err) => console.log(err.message));
-    fetch("https://newscatcher.p.rapidapi.com/v1/sources?topic=news&lang=en", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "784160c21emsh489f7d8d6d4c8bdp1f4e3djsn8a68a1315e74",
-		"x-rapidapi-host": "newscatcher.p.rapidapi.com"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+    
     console.log(this.posts);
   },
 }
 </script>
 
 <style scoped>
-/* #app {
-  font-family: "Roboto", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  margin-top: 60px;
-} */
 .home {
-  /* max-width: 1000px; */
   margin: 10px auto;
 }
 .blog-txt,
@@ -76,21 +56,10 @@ export default {
 .post-box {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  /* grid-template-columns: 1fr; */
   grid-gap: 25px;
   margin-top: 20px;
 }
-/* @media only screen and (min-width: 567px) {
-  .post-box {
-    grid-template-columns: 1fr 1fr;
-  }
-}
-@media only screen and (min-width: 798px) {
-  .post-box {
-    grid-template-columns: 1fr 1fr 1fr;
-  }
-} */
-/* @media only screen and (max-width: 798px) and (min-width: 567px) {
+@media only screen and (max-width: 798px) {
   .post-box {
     grid-template-columns: 1fr 1fr;
   }
@@ -99,5 +68,5 @@ export default {
   .post-box {
     grid-template-columns: 1fr;
   }
-} */
+}
 </style>
